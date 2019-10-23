e930a0050657e4d8546b01073bab8609266bc46b
Leonid Kulikov <eduleokul@gmail.com>
2019-10-23 21:42:54 +0300

    dilation_erosion: add erosion, fix dilation, add skimage analogs

--- dilation_erosion/diler.py
+++ dilation_erosion/diler.py
@@ -8,6 +8,7 @@ import numpy as np
 
 from skimage.io import imread, imsave
 from skimage import img_as_ubyte
+import skimage.morphology as skm
 
 
 if not sys.warnoptions:
@@ -43,6 +44,7 @@ parser.add_argument('-m', '--measure', nargs=2, type=check_positive, help='Measu
 parser.add_argument('-o', '--origin', nargs=2, type=check_non_negative, help='Origin for structure element',
                     required=True)
 parser.add_argument('-i', '--input', type=str, help='Path for input image', required=True)
+parser.add_argument('--skimage', action='store_true', help='Use skimage lib to perform morphology operations')
 args = parser.parse_args()
 
 # Parse arguments
@@ -52,6 +54,7 @@ measure = args.measure
 origin = args.origin
 guard(origin[0] < measure[0] and origin[1] < measure[1], '-o,--origin out of structure element')
 input_image_path = args.input
+skimage_flag = args.skimage
 
 
 def disk(measure, filled=True):
@@ -86,7 +89,7 @@ def structure_element(shape, measure, origin):
 
 def discretized_colors(image):
     discr_image = np.copy(image)
-    threshold = 100
+    threshold = 100  # Just random number
 
     for ix, iy in np.ndindex(discr_image.shape):
         discr_image[ix, iy] = 0 if discr_image[ix, iy] <= threshold else 255
@@ -102,18 +105,20 @@ def dilated_image(input_image, strel):
     for ix, iy in np.ndindex(input_image.shape):
         if input_image[ix, iy] == 255:
             # Set x-bounds to change in dil_im
-            _ix = ix - origin[0] if ix - origin[0] >= 0 else 0
-            _iy = iy - origin[1] if iy - origin[1] >= 0 else 0
+            __ix, __iy = ix - origin[0], iy - origin[1]
+            _ix = __ix if __ix >= 0 else 0
+            _iy = __iy if __iy >= 0 else 0
 
             # Set y-bounds to change in dil_im
-            ix__, iy__ = ix + strel_matrix.shape[1] - origin[0], iy + strel_matrix.shape[0] - origin[1]
-            ix_ = ix__ if ix__ < dil_im.shape[1] else dil_im.shape[1]
-            iy_ = iy__ if iy__ < dil_im.shape[0] else dil_im.shape[0]
+            ix__, iy__ = ix + strel_matrix.shape[0] - origin[0], iy + strel_matrix.shape[1] - origin[1]
+            ix_ = ix__ if ix__ < dil_im.shape[0] else dil_im.shape[0]
+            iy_ = iy__ if iy__ < dil_im.shape[1] else dil_im.shape[1]
 
             # Perform union operation
-            # import pdb; pdb.set_trace()
-            dil_im[_ix:ix_, _iy:iy_] += strel_matrix[origin[0]-(ix-_ix):origin[0]+(ix_-ix),
-                                                     origin[1]-(iy-_iy):origin[1]+(iy_-iy)]
+            dil_im[_ix:ix_, _iy:iy_] += strel_matrix[
+                origin[0] - abs(ix - _ix): origin[0] + abs(ix_ - ix),
+                origin[1] - abs(iy - _iy): origin[1] + abs(iy_ - iy)
+            ]
 
             # Normalize data
             mask = dil_im[_ix:ix_, _iy:iy_].astype(bool)
@@ -122,22 +127,70 @@ def dilated_image(input_image, strel):
     return dil_im
 
 
+def eroded_image(input_image, strel):
+    erod_im = np.zeros(input_image.shape, dtype=np.uint8)
+    origin = strel['origin']
+    strel_matrix = strel['matrix']
+
+    for ix, iy in np.ndindex(input_image.shape):
+        if input_image[ix, iy] == 255:
+            # Determ upper-left corner of input_image to check
+            __ix, __iy = ix - origin[0], iy - origin[1]
+            _ix = __ix if __ix >= 0 else 0
+            _iy = __iy if __iy >= 0 else 0
+
+            # Determ lower-right corner of input_image to check
+            ix__, iy__ = ix + strel_matrix.shape[0] - origin[0], iy + strel_matrix.shape[1] - origin[1]
+            ix_ = ix__ if ix__ < erod_im.shape[0] else erod_im.shape[0]
+            iy_ = iy__ if iy__ < erod_im.shape[1] else erod_im.shape[1]
+
+            # Select cells to erod_im
+            strel_check_segment = strel_matrix[origin[0]-(ix-_ix):origin[0]+(ix_-ix),
+                                               origin[1]-(iy-_iy):origin[1]+(iy_-iy)]
+            input_check_segment = input_image[_ix:ix_, _iy:iy_]
+            mask = strel_check_segment.astype(bool)
+            if (input_check_segment[mask] == strel_check_segment[mask]).all():
+                erod_im[ix, iy] = input_image[ix, iy]
+
+    return erod_im
+
+
+def construct_image(constructor, save_path=None, *args):
+    im = constructor(*args)
+
+    if save_path is not None:
+        imsave(save_path, img_as_ubyte(im))
+
+    return im
+
+
 def main():
     strel = structure_element(shape, measure, origin)
     input_image = discretized_colors(img_as_ubyte(imread(input_image_path, as_gray=True)))
+    imsave('discretized_input.png', input_image)
 
     if not (dilation or erosion):
         # Do both dilation and erosion
-        print('dilation and erosion')
+        if not skimage_flag:
+            construct_image(dilated_image, 'dilated_image.png', input_image, strel)
+            construct_image(eroded_image, 'eroded_image.png', input_image, strel)
+        else:
+            construct_image(skm.binary_dilation, 'skm_dilated_image.png', input_image, strel['matrix'])
+            construct_image(skm.binary_erosion, 'skm_eroded_image.png', input_image, strel['matrix'])
     else:
         # Do specified morphology operations
         if dilation:
-            # Do dilation
-            dil_im = dilated_image(input_image, strel)
-            imsave('dilated_image.png', dil_im)
+            if not skimage_flag:
+                construct_image(dilated_image, 'dilated_image.png', input_image, strel)
+            else:
+                construct_image(skm.binary_dilation, 'skm_dilated_image.png', input_image, strel['matrix'])
         if erosion:
-            # Do erosion
-            print('erosion')
+            if not skimage_flag:
+                construct_image(eroded_image, 'eroded_image.png', input_image, strel)
+            else:
+                construct_image(skm.binary_erosion, 'skm_eroded_image.png', input_image, strel['matrix'])
+
+    print('Specified images are successfuly created!')
 
 
 if __name__ == '__main__':
